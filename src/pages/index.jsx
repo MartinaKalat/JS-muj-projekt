3b0fd8e742e912c67cd3fc1d9420121c7f17922b
MartinaK <martinanemeckova8@gmail.com>
2025-04-22 19:35:14 +0200

    Upravy

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,17 +2,25 @@ import { render } from '@czechitas/render';
 import '../global.css';
 import './index.css';
 
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Footer Copyright</p>
+        </footer>
+    )
+    }
+
+
 document.querySelector('#root').innerHTML = render(
   <div className="container">
     <header>
       <div className="logo"></div>
-      <h1>Webová aplikace</h1>
+      <h1>Webová aplikace - updated</h1>
     </header>
     <main>
       <p>Startovací šablona pro webovou aplikaci v JavaScriptu s JSX. Vytvořeno pomocí <a href="https://www.npmjs.com/package/create-kodim-app">create-kodim-app</a>.</p>
     </main>
-    <footer>
-      <p>Czechitas, Digitální akademie: Web</p>
-    </footer>
+    <Footer />
   </div>
 );
